00a6503cbd67c07018127edacc8f494f9ddb7403
pepeyen <Administrador@DESKTOP-A5CSART.pitagoras.apollo.br>
2020-08-19 10:44:21 -0300

    Fixed missing core components

---
+++ src/components/Nodes/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+//Styles
+import './nodes.scss';
+
+function Nodes() {
+    const recommendationsTracks = useSelector(state => state.recommendations.tracks);
+    const recommendationsIsErrorless = useSelector(state => state.recommendations.isErrorLess);
+    
+    
+    if(recommendationsIsErrorless === true){
+        return(
+            recommendationsTracks.map(function(item, i){
+                return(
+                    <li 
+                        className="node"
+                        key={i}
+                    >
+                        <a 
+                            className="node__link"
+                            href={item.external_urls.spotify}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {item.name}
+                        </a>
+                    </li>  
+                ) 
+            })
+        );
+    }else{
+        return(
+            <div className="node">
+                <div className="node__feedback">Nothing was found</div>
+            </div>
+        )
+    }
+    
+}
+
+export default Nodes;
